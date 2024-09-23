a93e730d5446d352ad70de4f6952c524caa20fae
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-09-23 15:19:22 +0530

    add all required features

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
+import Table from "./Table";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [userData, setUserData] = useState([]);
 
-export default App
+  useEffect(() => {
+    setUserData(data);
+  }, [data]);
+
+  const handleClick = async () => {
+    try {
+      const response = await axios.get("https://reqres.in/api/users");
+      setData(response.data.data);
+    } catch (err) {
+      console.log("opps..!!. There was an error : ", err);
+    }
+  };
+
+  return (
+    <div id="main">
+      <div className="header">
+        <h1>Blue whales</h1>
+        <button id="btn" onClick={handleClick}>
+          Get User List
+        </button>
+      </div>
+      <Table data={userData} />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Table.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Table = ({ data }) => {
+  return (
+    <div className="table">
+      <table>
+        <thead>
+          <tr className="table-heading">
+            <th>First Name</th>
+            <th>last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 ? (
+            data.map((user) => {
+              return (
+                <tr key={user.id} className="body-rows">
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                  <td>{user.email}</td>
+                  <td className="avatar">
+                    <img src={user.avatar} />
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan="4" className="no-data">
+                <h3>No data found to display</h3>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default React.memo(Table);
